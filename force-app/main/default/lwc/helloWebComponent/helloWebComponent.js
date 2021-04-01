e023cf6b78574e66b11fc5812a9a2883155a12bc
rextejiri <rextejiri@gmail.com>
2021-04-01 03:21:59 -0500

    bear location component added bearsupervisor component added

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,7 +1,14 @@
+import SystemModstamp from '@salesforce/schema/Account.SystemModstamp';
 import { LightningElement } from 'lwc';
 
 export default class HelloWebComponent extends LightningElement {
     greeting = "Trailblazer";
+
+    currentDate = new Date().toDateString();
+
+    get capitalizedGreeting(){
+        return `Hello ${this.greeting.toUpperCase()}!`
+    }
 
     handleGreetingChange(event) {
         this.greeting = event.target.value;
